89476139456431ec5ac1569067809d1427f8b28a
Jordan Lee <jklee7@gmail.com>
2017-02-02 11:41:41 +1100

    Added how to assign console output to variable

--- 2. Variables.sh
+++ 2. Variables.sh
@@ -3,8 +3,14 @@
 
 # Assigning values to variables
 varString="Hello World!"
-echo $varString
+echo $varString $'\n'
+
+# Assigning console output to variable
+echo "Output of ls -l /"
+varConsole="$(ls -l /)"
+echo $varConsole $'\n'
 
 # Reading user input into a variable
+echo "Enter some text"
 read varText
 echo "You typed $varText"
