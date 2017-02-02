fdc882ce4c5be30e1bd8ed42b2682aabf0f1bf94
Jordan Lee <jklee7@gmail.com>
2017-02-02 11:31:26 +1100

    Updated to show how to read user input into variables

--- 2. Variables.sh
+++ 2. Variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Demonstration of variables within Bash
 
+# Assigning values to variables
 varString="Hello World!"
-
 echo $varString
+
+# Reading user input into a variable
+read varText
+echo "You typed $varText"
